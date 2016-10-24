4caa7112747114b8f0bd7b743e6716486c7703e2
Shaurya Srivastava <shauryas481@gmail.com>
2016-10-23 23:53:44 -0700

    Created login, Create Account, Graphics, and Main Screens.

---
+++ ios/Brain Stimulator/Brain Stimulator/Login.swift
@@ -0,0 +1,32 @@
+//
+//  Login.swift
+//  Brain Stimulator
+//
+//  Created by Shaurya Srivastava on 10/23/16.
+//  Copyright © 2016 Shaurya Srivastava. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LogIn: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(LogIn.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
---
+++ ios/Brain Stimulator/Brain Stimulator/UniversalParams.swift
@@ -0,0 +1,9 @@
+//
+//  UniversalParams.swift
+//  Brain Stimulator
+//
+//  Created by Shaurya Srivastava on 10/23/16.
+//  Copyright © 2016 Shaurya Srivastava. All rights reserved.
+//
+
+import Foundation
